b6c3e98b5fdc0677d673cf562a9a627d2db3e942
aristotelesfl <fl.aristoteles@gmail.com>
2026-03-25 12:15:02 -0300

    [Backend] feat: create initial setup to backend project

---
+++ BACKEND/config/settings/local.py
@@ -0,0 +1,7 @@
+# ruff: noqa: E501
+from .base import *  # noqa: F403
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
