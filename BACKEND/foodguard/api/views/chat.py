f5679f4f2326d69bf108d5c980736dabe51cced4
aristotelesfl <fl.aristoteles@gmail.com>
2026-04-21 11:40:06 -0300

    remove chat update view

--- BACKEND/foodguard/api/views/chat.py
+++ BACKEND/foodguard/api/views/chat.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.generics import UpdateAPIView
 from rest_framework.generics import DestroyAPIView
 
 from foodguard.api.serializers.chat import ChatSerializer
@@ -11,12 +10,6 @@ class ChatListAPIView(ListAPIView):
     
     def get_queryset(self):
         return Chat.objects.filter(user=self.request.user).order_by('-created_at')
-
-class ChatUpdateAPIView(UpdateAPIView):
-    serializer_class = ChatSerializer
-    
-    def get_queryset(self):
-        return Chat.objects.filter(id=self.kwargs['chat_id'])
 
 class ChatDestroyAPIView(DestroyAPIView):
     serializer_class = ChatSerializer
