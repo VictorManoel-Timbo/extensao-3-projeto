7cac32b268b8e8901c988717c8170f3cefa06c7d
aristotelesfl <fl.aristoteles@gmail.com>
2026-04-28 21:38:29 -0300

    feat: implement anamnese views, serializers, and exceptions

---
+++ BACKEND/foodguard/api/serializers/anamnese.py
@@ -0,0 +1,31 @@
+from rest_framework.serializers import ModelSerializer
+
+from foodguard.core.models.anamnese import Anamnese
+from foodguard.api.exceptions.anamnese import AnamneseObjectiveRequiredException
+
+
+class AnamneseSerializer(ModelSerializer):
+    class Meta:
+        model = Anamnese
+        fields = ['previous_consultation', 'alcohol_intake', 'smoking', 'previous_consultation_objective',
+                  'previous_consultation_result', 'disease_history', 'medications', 'favorite_foods', 'food_aversions',
+                  'body_feeling', 'eating_style']
+
+    def validate(self, attrs):
+        is_previous = attrs.get(
+            'previous_consultation', 
+            getattr(self.instance, 'previous_consultation', False)
+        )
+        
+        objective = attrs.get(
+            'previous_consultation_objective', 
+            getattr(self.instance, 'previous_consultation_objective', '')
+        )
+        
+        if isinstance(objective, str):
+            objective = objective.strip()
+
+        if is_previous and not objective:
+            raise AnamneseObjectiveRequiredException()
+        
+        return attrs
---
+++ BACKEND/foodguard/api/views/anamnese.py
@@ -0,0 +1,23 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from django.shortcuts import get_object_or_404
+
+from foodguard.api.serializers.anamnese import AnamneseSerializer
+from foodguard.core.models.anamnese import Anamnese
+from foodguard.api.exceptions.anamnese import AnamneseHasAlreadyBeenCreatedException
+
+
+class AnamneseCreateView(CreateAPIView):
+    serializer_class = AnamneseSerializer
+
+    def perform_create(self, serializer):
+        if Anamnese.objects.filter(user=self.request.user).exists():
+            raise AnamneseHasAlreadyBeenCreatedException()
+        serializer.save(user=self.request.user)
+
+class AnamneseGetUpdateView(RetrieveUpdateAPIView):
+    serializer_class = AnamneseSerializer
+    http_method_names = ['get', 'patch', 'options']
+
+    def get_object(self):
+        return get_object_or_404(Anamnese, user=self.request.user)
