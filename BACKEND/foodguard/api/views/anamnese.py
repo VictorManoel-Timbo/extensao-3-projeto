aba2d5b90a00c7ebdc7294318a54b41b433d1350
Aristóteles Feitosa <125211543+aristotelesfl@users.noreply.github.com>
2026-04-30 22:10:36 -0300

    initial endpoints (#24)

---
+++ BACKEND/foodguard/api/views/anamnese.py
@@ -0,0 +1,34 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from django.shortcuts import get_object_or_404
+
+from foodguard.api.serializers.anamnese import AnamneseSerializer
+from foodguard.core.models.anamnese import Anamnese
+from foodguard.api.exceptions.anamnese import AnamneseHasAlreadyBeenCreatedException
+from foodguard.core.models.chat import Chat
+
+@extend_schema(summary="Cria uma anamnese para o usuário")
+class AnamneseCreateView(CreateAPIView):
+    serializer_class = AnamneseSerializer
+
+    def perform_create(self, serializer):
+        if Anamnese.objects.filter(user=self.request.user).exists():
+            raise AnamneseHasAlreadyBeenCreatedException()
+        serializer.save(user=self.request.user)
+
+@extend_schema(summary="Recupera ou atualiza a anamnese do usuário")
+class AnamneseGetUpdateView(RetrieveUpdateAPIView):
+    serializer_class = AnamneseSerializer
+    http_method_names = ['get', 'patch', 'options']
+
+    def get_object(self):
+        return get_object_or_404(Anamnese, user=self.request.user)
+
+    def perform_update(self, serializer):
+        anamnese = self.get_object()
+        Chat.objects.filter(
+            user=self.request.user, 
+            created_at__lte=anamnese.updated_at
+        ).update(is_active=False)
+
---
+++ BACKEND/foodguard/api/views/chat.py
@@ -0,0 +1,22 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import DestroyAPIView
+
+from foodguard.api.serializers.chat import ChatSerializer
+from foodguard.core.models.chat import Chat
+
+@extend_schema(summary="Lista chats do usuário")
+class ChatListAPIView(ListAPIView):
+    serializer_class = ChatSerializer
+    
+    def get_queryset(self):
+        return Chat.objects.filter(user=self.request.user).order_by('-created_at')
+
+@extend_schema(summary="Deleta um chat específico do usuário")
+class ChatDestroyAPIView(DestroyAPIView):
+    serializer_class = ChatSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'chat_id'
+
+    def get_queryset(self):
+        return Chat.objects.filter(user=self.request.user)
