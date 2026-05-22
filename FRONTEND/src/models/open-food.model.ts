7c35f20ea8f91fae54adb97670cb2e684affb56e
aristotelesfl <fl.aristoteles@gmail.com>
2026-05-21 22:44:40 -0300

    refactor: simplify Ingredient type and remove unused Nutriments and NutriscoreData types

--- FRONTEND/src/models/open-food.model.ts
+++ FRONTEND/src/models/open-food.model.ts
@@ -1,54 +1,17 @@
-type Ingredients = {
-  text: string;
-  percent_estimated: number;
+type Ingredient = {
+  id: string;
+  is_in_taxonomy: number;
+  percent_estimate: number;
   percent_max: number;
   percent_min: number;
-};
-
-type Nutriments = {
-  carbohydrates: number;
-  carbohydrates_100g?: number;
-  carbohydrates_unit?: string;
-  energy?: number;
-  energy_kcal?: number;
-  energy_kcal_100g?: number;
-  fat?: number;
-  fat_100g?: number;
-  fiber?: number;
-  fiber_100g?: number;
-  proteins?: number;
-  proteins_100g?: number;
-  salt?: number;
-  salt_100g?: number;
-  saturated_fat?: number;
-  saturated_fat_100g?: number;
-  sodium?: number;
-  sodium_100g?: number;
-  sugars?: number;
-  sugars_100g?: number;
-};
-
-type NutriscoreData = {
-  energy: number;
-  carbohydrates: number;
-  grade?: string;
-  score?: number;
+  text: string;
 };
 
 type ProductData = {
   product_name: string;
-  brands?: string;
-  categories?: string;
-  image_url?: string;
-  image_front_url?: string;
-  ingredients?: Ingredients[];
-  ingredients_text?: string;
-  nutriments: Nutriments;
-  nutriscore_data?: NutriscoreData;
-  nutriscore_grade?: string;
-  nutrition_grades?: string;
-  quantity?: string;
-  serving_size?: string;
+  additives_tags: string[];
+  allergens_tags: string[];
+  ingredients: Ingredient[];
 };
 
 export interface IOpenFoodProduct {
